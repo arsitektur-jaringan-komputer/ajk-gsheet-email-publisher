092a1000fea1370fa8e8dd98f78f60d8e2936e56
Arrizki16 <kamildeka123@gmail.com>
2023-04-17 23:05:22 +0700

    [UPDATE] write output to a txt file in realtime

--- mailer.py
+++ mailer.py
@@ -1,5 +1,4 @@
-import smtplib
-import ssl
+import smtplib, ssl, sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
@@ -26,17 +25,14 @@ FILL THE BODY HERE
         img = Image.open('template_sertifikat.png')
         draw = ImageDraw.Draw(img)
 
-        ## For recipient name
-        font = ImageFont.truetype("Inter-Bold.ttf", 60) 
+        font = ImageFont.truetype("Inter-Bold.ttf", 56) 
         text = f"{recipient['fullname'].upper()}"
 
-        img_width, img_height = img.size
-        text_width, text_height = draw.textsize(text, font)
-
+        img_width, _ = img.size
+        text_width, _ = draw.textsize(text, font)
         x = (img_width - text_width) / 2
-        draw.text((x,330), text, fill="black", font=font)
+        draw.text((x,315), text, fill="black", font=font)
 
-        ## Saving the output
         edited_certificate_file = f"output/{recipient['nrp']}_{recipient['fullname']}_certificate.png"
         img.save(edited_certificate_file, "PNG")
 
@@ -45,27 +41,29 @@ FILL THE BODY HERE
 
     def send_emails(self, recipients):
         context = ssl.create_default_context()
+        with open('output.txt', 'w') as f:
+            for recipient in recipients:
+                message = MIMEMultipart()
+                message["From"] = self.sender_email
+                message["To"] = recipient['email']
+                message["Subject"] = self.subject
+                message.attach(MIMEText(self.set_body(recipient), "plain"))
 
-        for recipient in recipients:
-            
-            message = MIMEMultipart()
-            message["From"] = self.sender_email
-            message["To"] = recipient['email']
-            message["Subject"] = self.subject
-            message.attach(MIMEText(self.set_body(recipient), "plain"))
+                with open(self.generate_certificate(recipient), 'rb') as file:
+                    img_data = file.read()
+                image = MIMEImage(img_data)
+                image.add_header('Content-Disposition', 'attachment', filename='certificate.png')
+                message.attach(image)
 
-            with open(self.generate_certificate(recipient), 'rb') as file:
-                img_data = file.read()
-            image = MIMEImage(img_data)
-            image.add_header('Content-Disposition', 'attachment', filename='certificate.png')
-            message.attach(image)
+                with smtplib.SMTP(self.smtp_server, self.port) as server:
+                    server.starttls(context=context)
+                    try:
+                        server.login(self.main_email, self.password)
+                    except smtplib.SMTPAuthenticationError:
+                        print("Failed to log in to the SMTP server. Please check your email address and password.")
+                    
+                    server.sendmail(self.sender_email, recipient['email'], message.as_string())
+                    print(f"Email sent to {recipient['fullname'].upper()} at {recipient['email']}", file=f)
 
-            with smtplib.SMTP(self.smtp_server, self.port) as server:
-                server.starttls(context=context)
-                try:
-                    server.login(self.main_email, self.password)
-                except smtplib.SMTPAuthenticationError:
-                    print("Failed to log in to the SMTP server. Please check your email address and password.")
-                
-                server.sendmail(self.sender_email, recipient['email'], message.as_string())
-                print(f"Email sent to {recipient['nickname']} at {recipient['email']}")
+                    f.flush()
+                    sys.stdout.flush()
